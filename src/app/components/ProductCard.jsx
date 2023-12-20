cb6a68d846337d140436ad5b5476406e108543ae
Spiro <spiroheqimi90@gmail.com>
2023-12-20 15:06:50 +0100

    --

--- src/app/components/ProductCard.jsx
+++ src/app/components/ProductCard.jsx
@@ -1,20 +1,23 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function ({ product }) {
-
   return (
     <div>
-      <div className="bg-red-200 flex flex-col w-full p-10 m-2">
-        <Image 
-          src={product.image}
-          alt={product.name}
-          width={400}
-          height={400}
-        />
-        <p>Category: {product.category}</p>
-        <p>Price: ${product.price}</p>
-        <p>{product.description}</p>
-      </div>
+      <Link href={product.link}>
+        <div className="flex flex-col items-start gap-5 w-full p-10 m-2">
+          <Image
+            src={product.image}
+            alt={product.name}
+            width={400}
+            height={400}
+          />
+          <div className="w-full flex flex-col items-center">
+            <p className="text-lg font-bold">{product.description}</p>
+            <p className="text-secondary-color font-bold"> ${product.price}</p>
+          </div>
+        </div>
+      </Link>
     </div>
   );
 }
--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,27 +1,29 @@
-"use client"
+"use client";
 
-import React, { useState } from 'react';
-import FilterSidebar from '../components/FilterSidebar'
-import ProductCard from '../components/ProductCard'
-import products from '../products.json'
-
+import React, { useState } from "react";
+import FilterSidebar from "../components/FilterSidebar";
+import ProductCard from "../components/ProductCard";
+import products from "../products.json";
 
 export default function page() {
   const [selectedCategories, setSelectedCategories] = useState([]);
   const [selectedPriceRanges, setSelectedPriceRanges] = useState([]);
 
   const filteredProducts = products.filter((product) => {
-    const isCategoryMatch = selectedCategories.length === 0 || selectedCategories.includes(product.category);
+    const isCategoryMatch =
+      selectedCategories.length === 0 ||
+      selectedCategories.includes(product.category);
 
     const price = product.price;
-    const isPriceRangeMatch = selectedPriceRanges.length === 0 || selectedPriceRanges.some((range) => {
-        const [min, max] = range.split('-').map(parseFloat);
+    const isPriceRangeMatch =
+      selectedPriceRanges.length === 0 ||
+      selectedPriceRanges.some((range) => {
+        const [min, max] = range.split("-").map(parseFloat);
         return price >= min && price <= max;
       });
 
     return isCategoryMatch && isPriceRangeMatch;
   });
-
 
   const handleFilter = (selectedCategories, selectedPriceRanges) => {
     setSelectedCategories(selectedCategories);
@@ -29,16 +31,18 @@ export default function page() {
   };
 
   return (
-    <main className="w-screen min-h-screen flex px-10">
-      <FilterSidebar onFilter={handleFilter} />
-      <div className="w-full h-full py-10 grid grid-cols-4 gap-5 justify-start items-start overflow-hidden">
-        {filteredProducts.map((product) => (
-          <div key={product.id} className="w-90">
-            <ProductCard key={product.id} product={product}/>
-          </div>
-        ))}
+    <main className="w-screen min-h-screen flex flex-col items-center gap-6 px-10">
+      <h1 className="text-2xl font-bold py-5"> Products </h1>
+      <div className="flex w-full h-full">
+        <FilterSidebar onFilter={handleFilter} />
+        <div className="w-full h-full py-10 grid grid-cols-4 gap-5 justify-start items-start overflow-hidden">
+          {filteredProducts.map((product) => (
+            <div key={product.id} className="w-90">
+              <ProductCard key={product.id} product={product} />
+            </div>
+          ))}
+        </div>
       </div>
     </main>
   );
-};
-
+}
