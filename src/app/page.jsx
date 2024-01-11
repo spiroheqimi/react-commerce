c522eb632866a01dcdbaa4e910f2312db6d8b459
spiroheqimi <spiroheqimi90@gmail.com>
2024-01-11 15:27:00 +0100

    - test

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import menimg from "public/men-collection.png";
+import menimg from  "/public/men-collection.png";
 import { FiPhoneCall } from "react-icons/fi";
 import Slider from "./components/Slider";
 
